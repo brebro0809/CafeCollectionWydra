3fc2bb75450c1c834f70f147d5bd9abf2573553e
Brenden W <brendenwwydra@gmail.com>
2023-09-21 21:41:39 -0500

    mvp goal

--- CafeCollectionWydra/ViewController.swift
+++ CafeCollectionWydra/ViewController.swift
@@ -11,14 +11,22 @@ class ViewController: UIViewController {
 
 
     @IBOutlet weak var mainText: UITextView!
+    @IBOutlet weak var cartText: UITextView!
+    @IBOutlet weak var itemInput: UITextField!
+    @IBOutlet weak var amountInput: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    var items = ["coffee", "tea", "espresso", "bagel", "donut"]
+    var prices = [5.00, 3.50, 3.00, 5.00, 6.75]
+    var cart: [Int:Int] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
         
         mainText.isEditable = false
-        
-        let items = ["coffee", "tea", "espresso", "bagel", "donut"]
-        let prices = [5.00, 3.50, 3.00, 5.00, 6.75]
         
         mainText.text = ""
         
@@ -27,5 +35,44 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func cartPressed(_ sender: UIButton) {
+        if let index = items.firstIndex(of: itemInput.text ?? "") {
+            if let amount = Int(amountInput.text ?? ""){
+                errorLabel.isHidden = true
+                
+                if let inCart = cart[index] {
+                    errorLabel.text = "Item Already In Cart :)"
+                    errorLabel.isHidden = false
+                } else {
+                    cart[index] = amount
+                    updateCart()
+                }
+                
+            } else {
+                errorLabel.text = "Enter Valid Input :("
+                errorLabel.isHidden = false
+            }
+        }
+        else{
+            errorLabel.text = "Enter Valid Input :("
+            errorLabel.isHidden = false
+        }
+    }
+    
+    func updateCart() {
+        cartText.text = ""
+        var total = 0.0
+        
+        for(item, amount) in cart {
+            cartText.text += "\(items[item]) X \(amount) \n"
+            total += prices[item] * Double(amount)
+        }
+        totalLabel.text = "Cart Total: $\(total)"
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
 }
 
